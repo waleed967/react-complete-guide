3a86a9bb32cde6dcecdec5bebc712c2fb885d89b
waleed967 <waleedqureshi90@yahoo.com>
2020-10-07 16:30:10 +0500

    OutSideReturn

--- src/App.js
+++ src/App.js
@@ -3,78 +3,80 @@ import React, { Component } from 'react';
 import './App.css';
 // import UserInput from './UserInput/UserInput';
 // import UserOutput from './UserOutput/UserOutput'
-// import Person from './Person/Person'
+import Person from './Person/Person'
 // import List from './List/List'
 class App extends Component {
-    constructor(props) {
-        super(props)
+    // constructor(props) {
+    //     super(props)
 
-        this.state =
+    // this.
+    state =
         {
 
 
 
-            // persons: [
-            // {
-            value: " ",
-        }
-        // , age: "25"
-        // },
-        // {
-        //     name: "Adeel", age: "26"
-        // },
-        // {
-        //     name: "Talha", age: "27"
-        // }
-        // ],
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this)
-    };
+            persons: [
+                {
+                    //     value: " ",
+                    // }
+                    name: "Waleed", age: "25"
+                },
+                {
+                    name: "Adeel", age: "26"
+                },
+                {
+                    name: "Talha", age: "27"
+                }
+            ],
+            showPersons: false
+            //         this.handleChange = this.handleChange.bind(this);
+            // this.handleSubmit = this.handleSubmit.bind(this)
+        };
 
 
 
-    // switchNameHandler = (newName) => {
+    switchNameHandler = (newName) => {
 
-    //     this.setState({
+        this.setState({
 
-    // persons: [
-    // { 
-    // name: newName
-    // , age: 28
-    // },
-    // { name: 'Zia', age: 30 },
+            persons: [
+                {
+                    name: newName
+                    , age: 28
+                },
+                { name: 'Zia', age: 30 },
 
-    // { name: 'Haider', age: 31 },
-
-
-
-    // ]
+                { name: 'Haider', age: 31 }
 
 
-    //     });
+
+            ]
 
 
-    // }
-    handleChange = (event) => {
-        this.setState({ value: event.target.value });
+        });
 
 
     }
+    // handleChange = (event) => {
+    //     this.setState({ value: event.target.value });
 
-    handleSubmit = (event) => {
-        alert('Values are Submited' + this.state.value);
-        event.preventDefault();
-
-
-    }
-
-
-    // togglePersonHandler = () => {
-
-    //     const doesShow = this.state.showPersons;
-    //     this.setState({ showPersons: !doesShow });
 
     // }
+
+    // handleSubmit = (event) => {
+    //     alert('Values are Submited' + this.state.value);
+    //     event.preventDefault();
+
+
+    // }
+
+
+    togglePersonHandler = () => {
+
+        const doesShow = this.state.showPersons;
+        this.setState({ showPersons: !doesShow });
+
+    }
     // nameChangeHandler = (event) => {
     //     this.setState({
     //         persons: [
@@ -105,26 +107,36 @@ class App extends Component {
     // }
 
     render() {
+
+        let persons = null;
+        if (this.state.showPersons) {
+            persons = (<div>
+
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, 'Ahmed')}></Person>
+            </div>)
+
+
+
+        }
         return (
 
             <div className="App" >
                 <header className="App-header" >
-                    <form onSubmit={this.handleSubmit}>
+                    {/* <form onSubmit={this.handleSubmit}>
                         <label>
                             Values:
 <input type="text" value={this.state.value} onChange={this.handleChange} />
                         </label>
                         <input type="submit" value="Submit" />
-                    </form>
-                    {/* <button onClick={this.changeNumHandler}>Button</button>
-                    <List num={this.state.num}></List> */}
-                    {/* { */}
-                    {/* this.state.showPersons === true ? */}
-                    {/* <div> */}
+                    </form> */}
+                    <button onClick={this.togglePersonHandler}>Button</button>
+                    {persons}
+                    {/* <List num={this.state.num}></List> */}
+                    {/* {
+                        this.state.showPersons === true ? */}
 
-                    {/* <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, 'Ahmed')}></Person> */}
-                    {/* </div> : null */}
+                    {/* : null */}
                     {/* } */}
                 </header >
 
