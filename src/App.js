de79f5749728a66badf4ce3f640413d684a78bc6
waleed967 <waleedqureshi90@yahoo.com>
2020-10-07 16:55:16 +0500

    GoodPracticeForDeleteHandler

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
     }
 
     deletePersonHandler = (personIndex) => {
-        const persons = this.state.persons;
+        const persons = this.state.persons.slice();
         persons.splice(personIndex, 1);
         this.setState({ persons: persons })
 
