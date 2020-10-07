8f37fcaa61d3ffb892c2697ec7aea3ab4bfbaf49
waleed967 <waleedqureshi90@yahoo.com>
2020-10-07 14:57:39 +0500

    InputSubmit

--- src/App.js
+++ src/App.js
@@ -3,64 +3,78 @@ import React, { Component } from 'react';
 import './App.css';
 // import UserInput from './UserInput/UserInput';
 // import UserOutput from './UserOutput/UserOutput'
-import Person from './Person/Person'
+// import Person from './Person/Person'
+import List from './List/List'
 class App extends Component {
+    constructor(props) {
+        super(props)
 
-
-    state =
-
+        this.state =
         {
 
-            persons: [
-                {
 
-                    name: "Waleed", age: "25"
 
-                },
-                {
-
-                    name: "Adeel", age: "26"
-
-                },
-                {
-
-                    name: "Talha", age: "27"
-
-                }
-
-            ],
-            showPersons: false
-
+            // persons: [
+            // {
+            value: " ",
         }
+        // , age: "25"
+        // },
+        // {
+        //     name: "Adeel", age: "26"
+        // },
+        // {
+        //     name: "Talha", age: "27"
+        // }
+        // ],
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    };
 
 
 
-    switchNameHandler = (newName) => {
+    // switchNameHandler = (newName) => {
 
-        this.setState({
+    //     this.setState({
 
-            persons: [
-                { name: newName, age: 28 },
-                { name: 'Zia', age: 30 },
+    // persons: [
+    // { 
+    // name: newName
+    // , age: 28
+    // },
+    // { name: 'Zia', age: 30 },
 
-                { name: 'Haider', age: 31 },
+    // { name: 'Haider', age: 31 },
 
 
 
-            ]
+    // ]
 
 
-        });
+    //     });
+
+
+    // }
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
 
 
     }
 
-    togglePersonHandler = () => {
+    handleSubmit = (event) => {
+        alert('Num Submitted' + this.state.value);
+        event.preventDefault();
 
-        const doesShow = this.state.showPersons;
-        this.setState({ showPersons: !doesShow });
 
     }
+
+
+    // togglePersonHandler = () => {
+
+    //     const doesShow = this.state.showPersons;
+    //     this.setState({ showPersons: !doesShow });
+
+    // }
     // nameChangeHandler = (event) => {
     //     this.setState({
     //         persons: [
@@ -95,16 +109,23 @@ class App extends Component {
 
             <div className="App" >
                 <header className="App-header" >
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name:
+<input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                    </form>
+                    {/* <button onClick={this.changeNumHandler}>Button</button>
+                    <List num={this.state.num}></List> */}
+                    {/* { */}
+                    {/* this.state.showPersons === true ? */}
+                    {/* <div> */}
 
-                    <button onClick={this.togglePersonHandler}>Button</button>
-                    {
-                        this.state.showPersons === true ?
-                            <div>
-                                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-                                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-                                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, 'Ahmed')}></Person>
-                            </div> : null
-                    }
+                    {/* <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age} click={this.switchNameHandler.bind(this, 'Ahmed')}></Person> */}
+                    {/* </div> : null */}
+                    {/* } */}
                 </header >
 
             </div >
---
+++ src/List/List.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// const list = (props) => {
+//     return (
+
+
+
+
+
+//     )
+// };
+// export default list;
